de357712f5154b4f274789df40302b1d1327d88b
Laurentiu Ciocan <lciocan@ms3-inc.com>
2021-02-11 16:12:45 +0200

    expanded rules list and facts dictionaries

--- storage.py
+++ storage.py
@@ -9,7 +9,19 @@ facts_dictionary = {
   "G": "Is Heavy",
   "H": "Is a hired mafia goon",
   "I": "Wears blue",
-  "J": "Is a patrolling policeman"
+  "J": "Is a patrolling policeman",
+
+  "K": "Wears Pink",
+  "O": "Is a member of the BluePink Rock Band",
+  "P": "Is a singer",
+  "L": "Looks like a cartoon animal",
+  "M": "Has fur",
+  "N": "Is pretty beastly",
+  "Q": "Is a member of this town",
+  "R": "Is someone you can see on the streets daily",
+  "S": "Is a Looney living here",
+  "T": "Has a MOVING OUT truck outside of his house",
+  "U": "Is a Looney that is moving out of the town"
 }
 
 #RULES
@@ -19,7 +31,14 @@ rules_list = [
   "CD|F=",
   "EG>",
   "FG+H=",
-  "FI+J="
+  "FI+J=",
+  "KI|O=",
+  "OP>",
+  "MN>",
+  "LN+Q=",
+  "QR>",
+  "RS>",
+  "TQ+U="
 ]
 
 def evaluate_and(operandNodes, resultNode):
